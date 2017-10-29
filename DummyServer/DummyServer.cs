b2745ce4427b80834370a22d2d9f0544860c9b7a
Lee Kwenwon <highfence@naver.com>
2017-10-30 02:12:22 +0900

    [ADD] Receive까지 완성

--- DummyServer/DummyServer.cs
+++ DummyServer/DummyServer.cs
@@ -72,7 +72,55 @@ namespace DummyServer
 
 		void OnAcceptCompleted(object sender, SocketAsyncEventArgs e)
 		{
+			if (e.SocketError == SocketError.Success)
+			{
+				Socket clientSocket = e.AcceptSocket;
+				// Nagle 알고리즘 해제인듯
+				clientSocket.NoDelay = true;
 
+				SocketAsyncEventArgs receiveArgs = new SocketAsyncEventArgs();
+				receiveArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
+
+				var pending = clientSocket.ReceiveAsync(receiveArgs);
+				if (!pending)
+				{
+					ProcessReceive(receiveArgs);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Failed to accept cilent : " + e.SocketError);
+			}
+
+			this.flowControllEvent.Set();
+		}
+
+		void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
+		{
+			if (e.LastOperation == SocketAsyncOperation.Receive)
+			{
+				ProcessReceive(e);
+
+				return;
+			}
+		}
+
+		void ProcessReceive(SocketAsyncEventArgs e)
+		{
+			if (e.BytesTransferred > 0 && e.SocketError == SocketError.Success)
+			{
+				var buffer = e.Buffer;
+
+				bool pending = e.ConnectSocket.ReceiveAsync(e);
+				if (!pending)
+				{
+					ProcessReceive(e);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Process receive error");
+			}
 		}
     }
 }
